474597cf0fa56e2edeee59ec7d6af3b52dfe7cca
Cameron Fraser <cameron.fraser@systematicmedicine.com>
2025-06-27 10:20:36 +1000

    Added bash script to initiate pipeline, improving reproducibility between runs

---
+++ run_pipeline.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# --- run_pipeline.sh ---
+#
+# Snakemake command to run the codec-opensource pipeline
+#
+# Capturing this command in a file improves tracability and reproducibility between runs
+#
+# Author: Cameron Fraser
+# Date: 27-Jun-2025
+#
+
+snakemake \
+    --snakefile Snakefile_dev_CF \
+    --configfile config/config.yaml \
+    --cores all \
+    --keep-going \
+    --verbose \
+    --reason \
+    --stats metrics/pipeline_stats.json \
+    2>&1 | tee metrics/snakemake.log
