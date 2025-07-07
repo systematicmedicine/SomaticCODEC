d964a3ef5f43e2422a5aa7f345feb712c01877df
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-07-07 17:31:26 +1000

    Switched to Chr21 for ms test, having memory issues

--- tests/test_ms_fastq_to_bed.py
+++ tests/test_ms_fastq_to_bed.py
@@ -20,9 +20,9 @@ def test_ms_outputs(clean_workspace_fixture):
     target_dir = Path("tmp/downloads")
     target_dir.mkdir(exist_ok=True)
 
-    files_to_copy = [f"micro_GRCh38_Chr1_1Mbp.fna",
-                     f"ms_Chr1_100reads_r1.fastq.gz",
-                     f"ms_Chr1_100reads_r2.fastq.gz",
+    files_to_copy = [f"GRCh38_Chr21.fna",
+                     f"ms_Chr21_10000reads_r1.fastq.gz",
+                     f"ms_Chr21_10000reads_r2.fastq.gz",
                      f"GRCh38_alldifficultregions_10lines.bed",
                      f"gnomad_common_af01_merged_10lines.bed"
                      ]
@@ -49,11 +49,12 @@ def test_ms_outputs(clean_workspace_fixture):
     ms_samples = pd.read_csv("tests/configs/test_ms_outputs_samples.csv")["ms_sample"].to_list()
 
     # Load files to check
-    files_to_check = pd.read_csv("tests/configs/ms_intermediate_files.csv")
+    outputs_to_check = pd.read_csv("tests/configs/ms_output_files.csv")
 
-    for sample in ms_samples:
-        for _, row in files_to_check.iterrows():
-            path_str = row["file_path"].format(sample=sample) if "{sample}" in row["file_path"] else row["file_path"]
+    for ms_sample in ms_samples:
+        # Check intermediate files
+        for _, row in outputs_to_check[outputs_to_check["file_type"] == "intermediate"].iterrows():
+            path_str = row["file_path"].format(ms_sample=ms_sample) if "{ms_sample}" in row["file_path"] else row["file_path"]
             path = Path(path_str)
 
             # Check that file exists
@@ -62,12 +63,22 @@ def test_ms_outputs(clean_workspace_fixture):
             # Check that file is not empty
             assert path.stat().st_size > 0, f"Empty file: {path}"
 
-            # Check that tabular files have data rows
-            if path.suffix in [".csv", ".tsv", ".txt"]:
-                sep = "\t" if path.suffix in [".tsv", ".txt"] else ","
-                df = pd.read_csv(path, sep=sep, comment="#")
-                assert len(df) > 0, f"File has no data rows: {path}"
+            # Check that intermediate files have data rows
+            if path.suffix in [".bed"]:
+                try:
+                    df = pd.read_csv(path, sep="\t", comment="#", header = None)
+                    assert len(df) > 0, f"File has header but no data rows: {path}"
+                except pd.errors.EmptyDataError:
+                    assert False, f"File has header but no data rows: {path}"
+        
+        # Check metrics files
+        for _, row in outputs_to_check[outputs_to_check["file_type"] == "metric"].iterrows():
+            path_str = row["file_path"].format(ms_sample=ms_sample) if "{ms_sample}" in row["file_path"] else row["file_path"]
+            path = Path(path_str)
 
+            # Check that file exists
+            assert path.exists(), f"Missing file: {path}"
 
-    
+            # Check that file is not empty
+            assert path.stat().st_size > 0, f"Empty file: {path}"    
         
