af8167b57d3e1e829064c452406289929de21bf6
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-07-08 14:22:32 +1000

    Updated file list and pipeline test to include ex, to be tested with ex toy data

--- tests/count_data_points.py
+++ tests/count_data_points.py
@@ -11,17 +11,17 @@ from pathlib import Path
 import pysam
 
 # Gets list of all files to be checked
-def get_all_file_paths(outputs_to_check, ms_samples):#, ex_lanes, ex_samples):
+def get_all_file_paths(outputs_to_check, ms_samples, ex_lanes, ex_samples):
     all_files = set()
     for _, row in outputs_to_check.iterrows():
-        for sample in ms_samples: #+ ex_lanes + ex_samples:
+        for sample in ms_samples + ex_lanes + ex_samples:
             path_template = row["file_path"]
             if "{ms_sample}" in path_template:
                 path_str = path_template.format(ms_sample=sample)
-            #elif "{ex_lane}" in path_template:
-            #    path_str = path_template.format(ex_lane=sample)
-            #elif "{ex_sample}" in path_template:
-            #    path_str = path_template.format(ex_sample=sample)
+            elif "{ex_lane}" in path_template:
+                path_str = path_template.format(ex_lane=sample)
+            elif "{ex_sample}" in path_template:
+                path_str = path_template.format(ex_sample=sample)
             else:
                 path_str = path_template
             all_files.add(path_str)
@@ -122,4 +122,18 @@ def count_bcf_data_points(path):
     with pysam.VariantFile(path, "rb") as file:
         for _ in file.fetch():
             count += 1
+    return count
+
+# Counts the number of sequences in a FASTA file
+def count_fasta_data_points(path):
+    path = Path(path)
+    open_func = open
+    if str(path).endswith(".gz"):
+        open_func = gzip.open
+
+    count = 0
+    with open_func(path, 'rt') as file:
+        for line in file:
+            if line.startswith('>'):
+                count += 1
     return count
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -14,7 +14,7 @@ import pandas as pd
 import shutil
 import count_data_points
 
-# Tests if non-empty BED and metrics files can be created from raw ms FASTQ files
+# Tests if all pipeline output files can be created from raw FASTQ files
 def test_pipeline_outputs(clean_workspace_fixture):
 
     # Copy files into tmp/downloads
@@ -25,7 +25,9 @@ def test_pipeline_outputs(clean_workspace_fixture):
                      f"ms_Chr21_10000reads_r1.fastq.gz",
                      f"ms_Chr21_10000reads_r2.fastq.gz",
                      f"GRCh38_alldifficultregions_10lines.bed",
-                     f"gnomad_common_af01_merged_10lines.bed"
+                     f"gnomad_common_af01_merged_10lines.bed",
+                     f"ex_Chr21_10000reads_r1.fastq.gz",
+                     f"ex_Chr21_10000reads_r2.fastq.gz"
                      ]
 
     for filename in files_to_copy:
@@ -48,24 +50,24 @@ def test_pipeline_outputs(clean_workspace_fixture):
 
     # Load sample names
     ms_samples = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ms_sample"].to_list()
-    #ex_lanes = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_lane"].to_list()
-    #ex_samples = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_sample"].to_list()
+    ex_lanes = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_lane"].to_list()
+    ex_samples = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_sample"].to_list()
 
     # Load files to check
     outputs_to_check = pd.read_csv("tests/configs/test_pipeline_outputs_file_list.csv")
     checked_files = set()
 
-    for sample in ms_samples:# + ex_lanes + ex_samples:
+    for sample in ms_samples + ex_lanes + ex_samples:
         # Check output files
         for _, row in outputs_to_check.iterrows():
             path_str = (
                 row["file_path"].format(ms_sample=sample)
-                #if "{ms_sample}" in row["file_path"] else
-                #row["file_path"].format(ex_lane=sample)
-                #if "{ex_lane}" in row["file_path"] else
-                #row["file_path"].format(ex_sample=sample)
-                #if "{ex_sample}" in row["file_path"] else
-                #row["file_path"]
+                if "{ms_sample}" in row["file_path"] else
+                row["file_path"].format(ex_lane=sample)
+                if "{ex_lane}" in row["file_path"] else
+                row["file_path"].format(ex_sample=sample)
+                if "{ex_sample}" in row["file_path"] else
+                row["file_path"]
                 )
             
             checked_files.add(path_str)
@@ -76,7 +78,8 @@ def test_pipeline_outputs(clean_workspace_fixture):
 
             # Check that non-data files are not empty
             if path.suffix in [".amb", ".ann", ".bwt.2bit.64", ".pac", ".0123", 
-                               ".fai", ".dict", ".html", ".pdf", ".json", ".zip"]:
+                               ".fai", ".dict", ".html", ".pdf", ".json", ".zip",
+                               ".bai"]:
                 assert path.stat().st_size > 0, f"File is empty: {path}"
             else:
                 # Check that data files have data points
@@ -84,6 +87,6 @@ def test_pipeline_outputs(clean_workspace_fixture):
                 assert data_row_count > 1, f"File has no data points ({data_row_count}): {path_str}"
 
     # Check that all files in the file list have been checked
-    all_files = count_data_points.get_all_file_paths(outputs_to_check, ms_samples) #, ex_lanes, ex_samples)
+    all_files = count_data_points.get_all_file_paths(outputs_to_check, ms_samples, ex_lanes, ex_samples)
     missing_files = all_files - checked_files
     assert not missing_files, f"Files from file list not checked: {missing_files}"
