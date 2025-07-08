23c32130666652a3c56b6339fac496f52304e828
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-07-08 12:19:50 +1000

    New test structure working for all ms files

---
+++ tests/count_data_points.py
@@ -0,0 +1,125 @@
+"""
+--- count_data_points.py ---
+
+Functions for obtaining the number of data points from pipeline output files.
+
+Author: Joshua Johnstone
+
+"""
+import gzip
+from pathlib import Path
+import pysam
+
+# Gets list of all files to be checked
+def get_all_file_paths(outputs_to_check, ms_samples):#, ex_lanes, ex_samples):
+    all_files = set()
+    for _, row in outputs_to_check.iterrows():
+        for sample in ms_samples: #+ ex_lanes + ex_samples:
+            path_template = row["file_path"]
+            if "{ms_sample}" in path_template:
+                path_str = path_template.format(ms_sample=sample)
+            #elif "{ex_lane}" in path_template:
+            #    path_str = path_template.format(ex_lane=sample)
+            #elif "{ex_sample}" in path_template:
+            #    path_str = path_template.format(ex_sample=sample)
+            else:
+                path_str = path_template
+            all_files.add(path_str)
+    return all_files
+
+# Determines which data counting function to call based on file suffix
+def count_data_points(path):
+    path = Path(path)
+    suffixes = "".join(path.suffixes[-2:])
+    suffix = path.suffix
+
+    if suffixes == ".vcf.gz":
+        return count_vcf_data_points(path)
+    
+    if suffixes == ".fastq.gz":
+        return count_fastq_data_points(path)
+    
+    if suffix == ".bed":
+        return count_bed_data_points(path)
+    
+    elif suffix == ".vcf":
+        return count_vcf_data_points(path)
+    
+    elif suffix in [".csv", ".tsv", ".txt"]:
+        return count_tabular_data_points(path)
+    
+    elif suffix == ".fastq" or suffix == ".fq":
+        return count_fastq_data_points(path)
+    
+    elif suffix == ".bam":
+        return count_bam_data_points(path)
+    
+    elif suffix == ".sam":
+        return count_sam_data_points(path)
+    
+    elif suffix == ".bcf":
+        return count_bcf_data_points(path)
+    
+    else:
+        raise ValueError(f"Unsupported file type for data point counting: {path.suffix}")
+
+# Counts the number of data rows in a BED file
+def count_bed_data_points(path):
+    with open(path, 'r') as file:
+        return sum(1 for _ in file)
+
+# Counts the number of data rows in a VCF file
+def count_vcf_data_points(path):
+    path = Path(path)
+    open_func = gzip.open if "".join(path.suffixes[-2:]) == ".vcf.gz" else open
+    count = 0
+    with open_func(path, 'rt') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                count += 1
+    return count
+
+# Counts the number of data rows in a tabular file
+def count_tabular_data_points(path):
+    count = 0
+    with open(path) as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                count += 1
+    return count
+
+# Counts the number of reads in a FASTQ file
+def count_fastq_data_points(path):
+    count = 0
+    open_func = open
+    if str(path).endswith(".gz"):
+        open_func = gzip.open
+
+    with open_func(path, 'rt') as file:
+        for _ in file:
+            count += 1
+    return count // 4
+
+# Counts the number of reads in a SAM file
+def count_sam_data_points(path):
+    count = 0
+    with open(path, 'r') as file:
+        for line in file:
+            if not line.startswith('@'):
+                count += 1
+    return count
+
+# Counts the number of reads in a BAM file
+def count_bam_data_points(path):
+    with pysam.AlignmentFile(path, "rb") as bam:
+        return sum(1 for _ in bam)
+    
+# Counts the number of variant records in a BCF file
+def count_bcf_data_points(path):
+    count = 0
+    with pysam.VariantFile(path, "rb") as file:
+        for _ in file.fetch():
+            count += 1
+    return count
---
+++ tests/test_pipeline.py
@@ -0,0 +1,89 @@
+
+"""
+--- test_pipeline.py ---
+
+Function for testing all outputs of the full pipeline.
+
+Author: Joshua Johnstone
+
+"""
+
+import subprocess
+from pathlib import Path
+import pandas as pd
+import shutil
+import count_data_points
+
+# Tests if non-empty BED and metrics files can be created from raw ms FASTQ files
+def test_pipeline_outputs(clean_workspace_fixture):
+
+    # Copy files into tmp/downloads
+    target_dir = Path("tmp/downloads")
+    target_dir.mkdir(exist_ok=True)
+
+    files_to_copy = [f"GRCh38_Chr21.fna",
+                     f"ms_Chr21_10000reads_r1.fastq.gz",
+                     f"ms_Chr21_10000reads_r2.fastq.gz",
+                     f"GRCh38_alldifficultregions_10lines.bed",
+                     f"gnomad_common_af01_merged_10lines.bed"
+                     ]
+
+    for filename in files_to_copy:
+            source = Path("tests/data") / filename
+            dest = target_dir / filename
+            shutil.copy(source, dest)
+
+    # Run snakemake
+    snakemake_cmd = [
+        "snakemake",
+        "-s", "tests/snakefiles/Snakefile_test_pipeline_outputs",
+        "--cores", "all",
+        "--configfile", "tests/configs/test_pipeline_outputs_config.yaml",
+        "--notemp",
+        "--forceall",
+        "--rerun-incomplete"
+    ]
+
+    subprocess.run(snakemake_cmd)
+
+    # Load sample names
+    ms_samples = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ms_sample"].to_list()
+    #ex_lanes = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_lane"].to_list()
+    #ex_samples = pd.read_csv("tests/configs/test_pipeline_outputs_samples.csv")["ex_sample"].to_list()
+
+    # Load files to check
+    outputs_to_check = pd.read_csv("tests/configs/test_pipeline_outputs_file_list.csv")
+    checked_files = set()
+
+    for sample in ms_samples:# + ex_lanes + ex_samples:
+        # Check output files
+        for _, row in outputs_to_check.iterrows():
+            path_str = (
+                row["file_path"].format(ms_sample=sample)
+                #if "{ms_sample}" in row["file_path"] else
+                #row["file_path"].format(ex_lane=sample)
+                #if "{ex_lane}" in row["file_path"] else
+                #row["file_path"].format(ex_sample=sample)
+                #if "{ex_sample}" in row["file_path"] else
+                #row["file_path"]
+                )
+            
+            checked_files.add(path_str)
+            path = Path(path_str)
+
+            # Check that file exists
+            assert path.exists(), f"Missing file: {path}"
+
+            # Check that non-data files are not empty
+            if path.suffix in [".amb", ".ann", ".bwt.2bit.64", ".pac", ".0123", 
+                               ".fai", ".dict", ".html", ".pdf", ".json", ".zip"]:
+                assert path.stat().st_size > 0, f"File is empty: {path}"
+            else:
+                # Check that data files have data points
+                data_row_count = count_data_points.count_data_points(path)
+                assert data_row_count > 1, f"File has no data points ({data_row_count}): {path_str}"
+
+    # Check that all files in the file list have been checked
+    all_files = count_data_points.get_all_file_paths(outputs_to_check, ms_samples) #, ex_lanes, ex_samples)
+    missing_files = all_files - checked_files
+    assert not missing_files, f"Files from file list not checked: {missing_files}"
