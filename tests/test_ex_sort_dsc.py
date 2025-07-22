61f9bd2c00d91fd7272f550bc123e7f8959895a0
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-07-22 15:56:02 +1000

    Added test for ex_sort_dsc

--- tests/test_ex_sort_dsc.py
+++ tests/test_ex_sort_dsc.py
@@ -1,7 +1,7 @@
 """
---- test_ex_remap_dsc.py
+--- test_ex_sort_dsc.py
 
-Tests the rule ex_remap_dsc
+Tests the rule ex_sort_dsc
 
 Authors:
     - Chat-GPT
@@ -12,17 +12,17 @@ import glob
 from utils.bam_stats import count_bam_data_points
 
 def test_read_counts_preserved(lightweight_test_run):
-     # Locate all pre-remapping BAM files
-    pre_files = glob.glob("tmp/*/*_unmap_dsc.bam")
+     # Locate all pre-sorting BAM files
+    pre_files = glob.glob("tmp/*/*_map_dsc_unsorted.bam")
     pre_counts = {Path(f).name: count_bam_data_points(f) for f in pre_files}
     total_pre_reads = sum(pre_counts.values())
 
-    # Locate all post-remapping BAM files
-    post_files = glob.glob("tmp/*/*_map_dsc_unsorted.bam")
+    # Locate all post-sorting BAM files
+    post_files = glob.glob("tmp/*/*_map_dsc.bam")
     post_counts = {Path(f).name: count_bam_data_points(f) for f in post_files}
     total_post_reads = sum(post_counts.values())
 
-    # Assertion 1: Total reads pre remapping == total reads post remapping
+    # Assertion 1: Total reads pre sorting == total reads post sorting
     assert total_post_reads == total_pre_reads, (
-        f"Post-remapping reads ({total_post_reads}) not equal to pre-remapping reads ({total_pre_reads})"
+        f"Post-sorting reads ({total_post_reads}) not equal to pre-sorting reads ({total_pre_reads})"
     )
