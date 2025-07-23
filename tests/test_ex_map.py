9a82d7654c5941749827251030174a23e9fb2404
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-07-23 16:10:55 +1000

    Added tests for ex alignment

--- tests/test_ex_map.py
+++ tests/test_ex_map.py
@@ -1,7 +1,7 @@
 """
---- test_ms_raw_alignment.py
+--- test_ex_map.py
 
-Tests the rule ms_raw_alignment
+Tests the rule ex_map
 
 Authors:
     - Chat-GPT
@@ -13,18 +13,18 @@ from utils.bam_utils import count_bam_data_points
 from utils.fastq_utils import count_fastq_data_points
 
 # Test that aligned read count is not greater than input read count
-def test_aligned_reads_less_than_input_reads():
-    ms_samples = pd.read_csv("tests/configs/lightweight_test_run/ms_samples.csv")["ms_sample"].to_list()
+def test_aligned_reads_less_than_input_reads(lightweight_test_run):
+    ex_samples = pd.read_csv("tests/configs/lightweight_test_run/ex_samples.csv")["ex_sample"].to_list()
 
-    for ms_sample in ms_samples:
+    for ex_sample in ex_samples:
         
         # Get input read count
-        fastq_r1 = Path(f"tmp/{ms_sample}/{ms_sample}_filter_r1.fastq.gz")
-        fastq_r2 = Path(f"tmp/{ms_sample}/{ms_sample}_filter_r2.fastq.gz")
+        fastq_r1 = Path(f"tmp/{ex_sample}/{ex_sample}_r1_filter.fastq.gz")
+        fastq_r2 = Path(f"tmp/{ex_sample}/{ex_sample}_r2_filter.fastq.gz")
         input_reads = count_fastq_data_points(fastq_r1) + count_fastq_data_points(fastq_r2)
 
         # Get aligned read count       
-        bam_file = Path(f"tmp/{ms_sample}/{ms_sample}_raw_map.bam")
+        bam_file = Path(f"tmp/{ex_sample}/{ex_sample}_map.bam")
         bam_reads = count_bam_data_points(bam_file)
 
         # Assert total aligned reads <= total input reads
