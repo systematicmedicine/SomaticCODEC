3ce83789d32fbda9941b5f9f248061c8ef36093b
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-08-08 14:48:30 +1000

    Added test for ex_demux_metrics_gini.py

--- scripts/ex_demux_metrics_gini.py
+++ scripts/ex_demux_metrics_gini.py
@@ -13,52 +13,57 @@ import numpy as np
 import re
 from collections import defaultdict
 import json
+import sys
 
-# Initiate logging
-sys.stdout = open(snakemake.log[0], "a")
-sys.stderr = open(snakemake.log[0], "a")
-print("[INFO] Starting ex_demux_metrics_gini.py")
+def main(snakemake):
+    # Initiate logging
+    sys.stdout = open(snakemake.log[0], "a")
+    sys.stderr = open(snakemake.log[0], "a")
+    print("[INFO] Starting ex_demux_metrics_gini.py")
 
-# Calculates Gini coefficient
-def gini_coefficient(x):
-    x = np.sort(np.array(x))
-    n = len(x)
-    cumulative_diffs = np.abs(np.subtract.outer(x, x)).sum()
-    return cumulative_diffs / (2 * n**2 * np.mean(x))
+    # Calculates Gini coefficient
+    def gini_coefficient(x):
+        x = np.sort(np.array(x))
+        n = len(x)
+        cumulative_diffs = np.abs(np.subtract.outer(x, x)).sum()
+        return cumulative_diffs / (2 * n**2 * np.mean(x))
 
-# Get paths
-demux_metrics_path = snakemake.input.demux_metrics
-json_out_path = snakemake.output.demux_gini
+    # Get paths
+    demux_metrics_path = snakemake.input.demux_metrics
+    json_out_path = snakemake.output.demux_gini
 
-# Get counts for each adaptor after demux
-trimmed_counts = defaultdict(int)
-with open(demux_metrics_path, "r") as f:
-        for line in f:
-            adapter_match = re.match(r"=== .* read: Adapter (S\d+) ===", line)
-            if adapter_match:
-                current_adapter = adapter_match.group(1)
-                continue
+    # Get counts for each adaptor after demux
+    trimmed_counts = defaultdict(int)
+    with open(demux_metrics_path, "r") as f:
+            for line in f:
+                adapter_match = re.match(r"=== .* read: Adapter (S\d+) ===", line)
+                if adapter_match:
+                    current_adapter = adapter_match.group(1)
+                    continue
 
-            trimmed_match = re.search(r"Trimmed:\s+([\d,]+) times", line)
-            if trimmed_match and current_adapter:
-                count = int(trimmed_match.group(1).replace(",", ""))
-                trimmed_counts[current_adapter] += count
+                trimmed_match = re.search(r"Trimmed:\s+([\d,]+) times", line)
+                if trimmed_match and current_adapter:
+                    count = int(trimmed_match.group(1).replace(",", ""))
+                    trimmed_counts[current_adapter] += count
 
-# Collate counts and calculate Gini coefficient
-output_data = {
-     "description": (
-    "Summary of adaptor counts and Gini coefficient for inequality between counts."
-    ),
-    "trimmed_counts": dict(trimmed_counts),
-    "gini_coefficient": round(gini_coefficient(list(trimmed_counts.values())), 3)
-}
+    # Collate counts and calculate Gini coefficient
+    output_data = {
+        "description": (
+        "Summary of adaptor counts and Gini coefficient for inequality between counts."
+        ),
+        "trimmed_counts": dict(trimmed_counts),
+        "gini_coefficient": round(gini_coefficient(list(trimmed_counts.values())), 3)
+    }
 
-# Write to JSON
-with open(json_out_path, "w") as out_f:
-    json.dump(output_data, out_f, indent=4)
+    # Write to JSON
+    with open(json_out_path, "w") as out_f:
+        json.dump(output_data, out_f, indent=4)
 
-print("[INFO] Completed ex_demux_metrics_gini.py")
+    print("[INFO] Completed ex_demux_metrics_gini.py")
 
+# Only run in Snakemake
+if __name__ == "__main__":
+    main(snakemake) 
 
 
 
