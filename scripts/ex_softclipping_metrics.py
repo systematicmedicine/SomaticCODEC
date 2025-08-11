15450f9deea65974d5feab620e54322e2211568b
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-08-11 13:58:13 +1000

    Added test_ex_softclipping_metrics.py

--- scripts/ex_softclipping_metrics.py
+++ scripts/ex_softclipping_metrics.py
@@ -8,6 +8,7 @@ Rule to be used exclusively with parent rule, ex_softclipping_metrics
 Authors:
     - Chat-GPT
     - Cameron Fraser
+    - Joshua Johnstone
 """
 
 # Import libraries
@@ -15,33 +16,33 @@ import pysam
 import json
 import numpy as np
 import os
+import sys
 
-# Initiate logging
-sys.stdout = open(snakemake.log[0], "a")
-sys.stderr = open(snakemake.log[0], "a")
-print("[INFO] Starting ex_total_read_loss.py")
-
-# Extract soft-clipped base counts per read from a BAM file
-def get_softclip_lengths(bam_path):
-    softclip_lengths = []
-
-    with pysam.AlignmentFile(bam_path, "rb") as bam:
-        for read in bam:
-            if read.cigartuples:
-                softclip_bases = sum(length for op, length in read.cigartuples if op == 4)
-                softclip_lengths.append(softclip_bases)
-
-    return softclip_lengths
-
-# Return a dict of specified percentiles from a list of values
-def calculate_percentiles(values, percentiles):
-    values_array = np.array(values)
-    return {
-        f"{p}th_percentile": int(np.percentile(values_array, p, interpolation="nearest")) for p in percentiles
-    }
-
-# Main logic
 def main(snakemake):
+    # Initiate logging
+    sys.stdout = open(snakemake.log[0], "a")
+    sys.stderr = open(snakemake.log[0], "a")
+    print("[INFO] Starting ex_total_read_loss.py")
+
+    # Extract soft-clipped base counts per read from a BAM file
+    def get_softclip_lengths(bam_path):
+        softclip_lengths = []
+
+        with pysam.AlignmentFile(bam_path, "rb") as bam:
+            for read in bam:
+                if read.cigartuples:
+                    softclip_bases = sum(length for op, length in read.cigartuples if op == 4)
+                    softclip_lengths.append(softclip_bases)
+
+        return softclip_lengths
+
+    # Return a dict of specified percentiles from a list of values
+    def calculate_percentiles(values, percentiles):
+        values_array = np.array(values)
+        return {
+            f"{p}th_percentile": int(np.percentile(values_array, p, method="nearest")) for p in percentiles
+        }
+
     bam_path = snakemake.input.dsc_final
     output_json = snakemake.output.file_path
 
