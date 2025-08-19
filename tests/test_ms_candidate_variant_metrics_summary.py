2eaf613558dd5cfcd94e454eba519c4c8f2894d7
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-08-19 10:13:47 +1000

    Updated germline variant rate calc

--- tests/test_ms_candidate_variant_metrics_summary.py
+++ tests/test_ms_candidate_variant_metrics_summary.py
@@ -13,17 +13,18 @@ import shutil
 from scripts.ms_candidate_variant_metrics_summary import main
 
 @pytest.mark.parametrize(
-    "mask_metrics_path, variant_metrics_path, fai_path, het_hom_ratio_path, sample_name, expected_metrics",
+    "mask_metrics_path, variant_metrics_path, het_hom_ratio_path, depth_hist_path, sample_name, expected_metrics",
     [
         (
             "tests/data/test_ms_candidate_variant_metrics_summary/mask_metrics.json",
             "tests/data/test_ms_candidate_variant_metrics_summary/variant_metrics.txt",
-            "tests/data/test_ms_candidate_variant_metrics_summary/reference.fai",
             "tests/data/test_ms_candidate_variant_metrics_summary/ms_het_hom_ratio.txt",
+            "tests/data/test_ms_candidate_variant_metrics_summary/depth_histogram.txt",
             "TestSample",
             {
-                "fraction_genome_over_depth_threshold": 0.5,
-                "germline_variant_rate": 0.005,
+                "callable_bases": 3000000,
+                "variants_called": 5000,
+                "germline_variant_rate": 0.0017,
                 "snv_indel_ratio": 9,
                 "insertion_deletion_ratio": 0.92,
                 "MNP_other_variants": 3,
@@ -33,24 +34,24 @@ from scripts.ms_candidate_variant_metrics_summary import main
         )
     ]
 )
-def test_ms_candidate_variant_metrics_summary(tmp_path, mask_metrics_path, variant_metrics_path, fai_path, het_hom_ratio_path, sample_name, expected_metrics):
+def test_ms_candidate_variant_metrics_summary(tmp_path, mask_metrics_path, variant_metrics_path, het_hom_ratio_path, depth_hist_path, sample_name, expected_metrics):
 
     mmp = tmp_path / "mask_metrics.json"
     vmp = tmp_path / "variant_metrics.txt"
-    fp = tmp_path / "reference.fai"
     hhp = tmp_path / "ms_het_hom_ratio.txt"
+    dhp = tmp_path / "depth_histogram.txt"
 
     shutil.copy(mask_metrics_path, mmp)
     shutil.copy(variant_metrics_path, vmp)
-    shutil.copy(fai_path, fp)
     shutil.copy(het_hom_ratio_path, hhp)
+    shutil.copy(depth_hist_path, dhp)
 
     class MockSnakemake:
         input = type("input", (), {
             "variant_metrics": str(vmp),
-            "fai": str(fp),
             "ms_het_hom_ratio": str(hhp),
-            "mask_metrics": str(mmp)
+            "mask_metrics": str(mmp),
+            "depth_hist": str(dhp)
         })
         params = type("params", (), {"sample": sample_name})
         output = type("output", (), {"summary": str(tmp_path / "summary.json")})
