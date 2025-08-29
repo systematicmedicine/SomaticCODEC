db024537342659e311465db4fa16f9740fc62d5a
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-08-29 11:46:58 +1000

    Added background monitoring of disk usage, memory usage, and cpu load to run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,12 +6,34 @@
 #
 # Capturing this command in a file improves tracability and reproducibility between runs
 #
-# Author: Cameron Fraser
-# Date: 27-Jun-2025
+# Authors: 
+#   - Cameron Fraser
+#   - Joshua Johnstone
 #
 
+set -e
+
+# Create directory for pipeline logs
 mkdir -p logs/pipeline
 
+# Start continuous system monitoring in the background
+echo "time,disk_used_GB,mem_used_GB,cpu_load" > logs/pipeline/system_usage.csv
+(
+    while true; do
+        now=$(date +"%Y-%m-%d %H:%M:%S")
+        disk_used_GB=$(df -BG / | tail -1 | awk '{gsub("G","",$3); print $3}')
+        mem_used_GB=$(free -g | awk '/Mem:/ {print $2}')
+        cpu_load=$(uptime | awk -F'load average:' '{print $2}' | cut -d',' -f1 | xargs)
+        echo "$now,$disk_used_GB,$mem_used_GB,$cpu_load" >> logs/pipeline/system_usage.csv
+        sleep 60
+    done
+) &
+MONITOR_PROCESS_ID=$!
+
+# Stop background monitoring when script exits (finishes or errors)
+trap "kill $MONITOR_PROCESS_ID" EXIT
+
+# Run Snakemake
 snakemake \
     --snakefile Snakefile \
     --configfile config/config.yaml \
