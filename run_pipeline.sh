c6169570c682454617fdc1fee91b4dc62685c002
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-09-01 14:27:26 +1000

    Abstracted resource monitoring into monitor_system_resources.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -17,28 +17,10 @@ set -e
 mkdir -p logs/pipeline
 
 # Start continuous resource monitoring in the background
-echo "time,disk_used_GB,disk_avail_GB,mem_used_GB,mem_avail_GB,cpu_load,cpu_avail" > logs/pipeline/system_resource_usage.csv
-(
-    while true; do
-        now=$(date +"%Y-%m-%d %H:%M:%S")
+MONITOR_RESOURCES_PID=$(./monitor_system_resources.sh)
 
-        disk_used_GB=$(df -BG / | tail -1 | awk '{gsub("G","",$3); print $3}')
-        disk_avail_GB=$(df -BG / | tail -1 | awk '{gsub("G","",$4); print $4}')
-
-        mem_used_GB=$(free -g | awk '/Mem:/ {print $3}')
-        mem_avail_GB=$(free -g | awk '/Mem:/ {print $7}')
-
-        cpu_load=$(uptime | awk -F'load average:' '{print $2}' | cut -d',' -f1 | xargs)
-        cpu_avail=$(($(nproc) - ${cpu_load%.*}))
-
-        echo "$now,$disk_used_GB,$disk_avail_GB,$mem_used_GB,$mem_avail_GB,$cpu_load,$cpu_avail" >> logs/pipeline/system_resource_usage.csv
-        sleep 60
-    done
-) &
-MONITOR_RESOURCES_PROCESS_ID=$!
-
-# Stop background monitoring when script exits (finishes or errors)
-trap "kill $MONITOR_RESOURCES_PROCESS_ID" EXIT
+# Stop background resource monitoring when pipeline finishes or crashes
+trap "kill $MONITOR_RESOURCES_PID" EXIT
 
 # Run Snakemake
 snakemake \
