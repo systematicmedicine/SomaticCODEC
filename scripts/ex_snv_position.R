57c4c199a750644bddaba12e4a55a9e24c68f3e4
Cameron Fraser <cameron.fraser@systematicmedicine.com>
2025-09-22 16:39:24 +1000

    Added metrics file for SNV genomic position

---
+++ scripts/ex_snv_position.R
@@ -0,0 +1,127 @@
+# =============================================================================
+#   ex_snv_position.R
+#
+#   Create a plot of the positional distribution of SNVs called by the pipeline
+#  
+#   Author: Cameron Fraser
+#
+# ==============================================================================
+
+
+# ------------------------------------------------------------------------------
+# Setup
+# ------------------------------------------------------------------------------
+
+# Load libraries
+library(dplyr)
+library(jsonlite)
+library(ggplot2)
+
+# Snakemake parameter injection
+VCF_PATH <- snakemake@input[["vcf_path"]]
+FAI_PATH <- snakemake@input[["index_path"]]
+METRICS_PATH <- snakemake@output[["metrics_json"]]
+PLOT_PATH <- snakemake@output[["metrics_plot"]]
+LOG_PATH <- snakemake@log[[1]]
+CONFIG <- snakemake@config
+
+# Start logging
+log_con <- file(LOG_PATH, open = "wt")
+sink(log_con, type = "message")
+message(sprintf("[INFO] Script started at %s\n", Sys.time()))
+
+
+# ------------------------------------------------------------------------------
+# Calculate genomic percentiles
+# ------------------------------------------------------------------------------
+
+genomic_percentiles <- function(fai_path, vcf_path){
+
+    # Load reference genome FAI
+    fai <- read.table(fai_path, header = FALSE, sep = "\t", stringsAsFactors = FALSE) %>%
+    rename(Chrom = V1, Length = V2) %>%
+    select(Chrom, Length) %>%
+    filter(Chrom %in% CONFIG$chroms$included_chromosomes)
+
+    # Load SNV VCF
+    vcf.header.line <- max(grep("^#CHROM", readLines(VCF_PATH)))
+
+    vcf <- read.table(vcf_path, header = TRUE, sep = "\t", stringsAsFactors = FALSE, comment.char = "", skip = vcf.header.line - 1, check.names = FALSE) %>%
+        rename(Chrom = `#CHROM`, Pos = POS) %>%
+        select(Chrom, Pos) %>%
+        left_join(fai, by = "Chrom") %>%
+        mutate(Percentile = Pos / Length * 100)
+}
+
+snv.pos <- genomic_percentiles(FAI_PATH, VCF_PATH)
+
+
+# ------------------------------------------------------------------------------
+# Calculate mean absolute deviation
+# ------------------------------------------------------------------------------
+
+mad_vs_uniform <- function(genomic_percentiles){
+    
+    # Expected distribution
+    expected <- seq(0, 100, length = length(genomic_percentiles))
+
+    # Sort percentiles vector
+    genomic_percentiles <- sort(genomic_percentiles)
+
+    # Calulate MAD
+    mad <- mean(abs(genomic_percentiles - expected))
+
+    # Round
+    mad <- round(mad, 2)
+
+    # Return result
+    return(mad)
+}
+
+chr.mad <- snv.pos %>%
+    group_by(Chrom) %>%
+    summarise(MAD = mad_vs_uniform(Percentile))
+
+
+# ------------------------------------------------------------------------------
+# Write out metrics file
+# ------------------------------------------------------------------------------
+
+metrics_json <- list(
+  description = "For each SNV, genomic position percentiles were calculated (0 = start, 50 = middle, 100 = end). These percentiles were compared to a uniform distribution, and the mean absolute deviation calculated",
+  chromosomes_MAD = setNames(as.list(chr.mad$MAD), chr.mad$Chrom),
+  max_MAD = max(chr.mad$MAD, na.rm = TRUE)
+)
+
+# Write JSON to disk
+write_json(metrics_json, METRICS_PATH, pretty = TRUE, auto_unbox = TRUE)
+
+# ------------------------------------------------------------------------------
+# Create plot
+# ------------------------------------------------------------------------------
+
+plt <- ggplot(data = snv.pos, aes(x = Chrom, y = Percentile, colour = Chrom)) +
+    geom_jitter(size = 0.5, alpha = 0.5, width = 0.1, height = 0) +
+    scale_y_continuous(limits = c(0, 100.0)) +
+    ggtitle(
+        "Positional distribution of called SNVs", 
+        paste(Sys.Date(), CONFIG$experiment$name)
+        ) +
+    xlab("") +
+    ylab("Genomic percentile") +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1), 
+        legend.position = "none")
+
+# Write plot to disk
+ggsave(PLOT_PATH, plt)
+
+
+# ---------------------------------------------------------------------------
+# Cleanup
+# ---------------------------------------------------------------------------
+
+# Release logging
+message(sprintf("[INFO] Script finished at %s\n", Sys.time()))
+sink(type = "message")
+close(log_con)
