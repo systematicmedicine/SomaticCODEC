53bea5176d9465be148f907c267879dd60ebebc8
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2025-10-23 09:11:01 +1100

    Revert "Merge pull request #222 from systematicmedicine/feature/germline-overlap-rate"
    This reverts commit 8a3e2c4b77f1f57349c965408986b4c7a1378a99, reversing
    changes made to 7c33133ecdb3854869c5def3b922f32732e39f4d.

--- scripts/ex_germline_contamination.py
+++ scripts/ex_germline_contamination.py
@@ -1,9 +1,9 @@
 """
---- ex_gnomAD_overlap.py
+--- ex_germline_contamination.py
 
 Determines how many called somatic variants are present in dataset of common germline variants
 
-Designed to be used exclusively with the rule "ex_gnomAD_overlap"
+Designed to be used exclusively with the rule "ex_germline_contamination"
 
 Authors:
     - Chat-GPT
@@ -19,11 +19,10 @@ def main(snakemake):
     # Initiate logging
     sys.stdout = open(snakemake.log[0], "a")
     sys.stderr = open(snakemake.log[0], "a")
-    print("[INFO] Starting ex_gnomAD_overlap.py")
+    print("[INFO] Starting ex_germline_contamination.py")
 
     # Inputs
     somatic_vcf = Path(snakemake.input.somatic_vcf)
-    somatic_all_vcf = Path(snakemake.input.somatic_all_vcf)
     germline_vcf = Path(snakemake.input.germline_vcf)
 
     # Outputs
@@ -71,53 +70,22 @@ def main(snakemake):
         )
         num_matches = len(result.stdout.strip().splitlines()) if result.stdout.strip() else 0
 
-    # --- Count number of bases assessable for somatic calling ---
-    evaluated_bases = 0
-
-    with open(somatic_all_vcf) as f:
-    # Check through all lines of the complete vcf (variants and no variants) for the following:
-        # Evaluated bases - total bases assessed for variants (ie. denominator)
-        for line in f:
-            if line.startswith("#"):
-                continue
-
-            cols = line.strip().split("\t")
-            assert len(cols) >= 9, f"Malformed line with too few columns:\n{line}"
-
-            sample_fmt = cols[8].split(":")
-            sample_vals = cols[9].split(":")
-            fmt = dict(zip(sample_fmt, sample_vals))
-            dp_fmt = int(fmt.get("DP", 0))
-
-            if dp_fmt > 0:
-                evaluated_bases += 1
-
-    # --- Calculate percent gnomAD overlap ---
-    percent_gnomAD_overlap = round(100 * num_matches / total_variants, 2) if total_variants > 0 else 0
-
-    # --- Calculate rate of gnomAD overlap ---
-    rate_gnomAD_overlap = round(num_matches / evaluated_bases, 6) if evaluated_bases > 0 else 0
+    # --- Calculate percent germline contamination ---
+    percent_germline_contamination = round(100 * num_matches / total_variants, 2) if total_variants > 0 else 0
 
     # --- Write metrics JSON ---
     metrics_file.parent.mkdir(parents=True, exist_ok=True)
     with open(metrics_file, "w") as f:
         json.dump({
-            "description": "Number and rate of called somatic variants that overlapp with known germline variants",
+            "description": "Number of called somatic variants overlapping known germline variants",
             "somatic_vcf": str(somatic_vcf),
-            "gnomAD_vcf": str(germline_vcf),
-            "total_evaluated_bases": {"description": "Number of unmasked bases with DP > 0 and BQ > min_base_quality",
-                                      "value": evaluated_bases},
-            "total_somatic_variants": {"description": "Number of called somatic variants",
-                                      "value": total_variants},
-            "total_gnomAD_matches": {"description": "Number of called somatic variants that overlap with gnomAD",
-                                      "value": num_matches},
-            "percent_gnomAD_overlap": {"description": "Percentage of called somatic variants that overlap with gnomAD",
-                                      "value": percent_gnomAD_overlap},
-            "rate_gnomAD_overlap": {"description": "Rate of SNVs that overlap with gnomAD per evalulated base",
-                                      "value": rate_gnomAD_overlap},
+            "germline_vcf": str(germline_vcf),
+            "total_variants": total_variants,
+            "germline_matches": num_matches,
+            "percent_germline_contamination": percent_germline_contamination
         }, f, indent=2)
 
-    print(f"[INFO] Completed ex_gnomAD_overlap.py")
+    print(f"[INFO] Completed ex_germline_contamination.py")
 
 if __name__ == "__main__":
     main(snakemake)
