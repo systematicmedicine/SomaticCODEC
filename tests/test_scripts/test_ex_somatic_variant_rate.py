9c85c8f1b2a488c73a8f5d8e912c0bc4a2c6de4b
Cameron Fraser <cameron.fraser@systematicmedicine.com>
2025-10-27 13:00:01 +1100

    Descriptions added to metrics file

--- tests/test_scripts/test_ex_somatic_variant_rate.py
+++ tests/test_scripts/test_ex_somatic_variant_rate.py
@@ -24,44 +24,45 @@ from scripts.ex_somatic_variant_rate import main
         (
             "tests/data/test_ex_somatic_variant_rate/variants.vcf",
             {
-                "starting_bases": 35,
-                "min-BQ": 20,
-                "min-MQ": 30,
-                "filtered_bases": 0,
-                "evaluated_bases": 35,
-                "num_snv_bases": 11,
-                "snv_rate": 0.3142857,
-                "snv_per_diploid": 1905743997,
+                "starting_bases": {"value": 35},
+                "min-BQ": {"value": 20},
+                "min-MQ": {"value": 30},
+                "filtered_bases": {"value": 0},
+                "evaluated_bases": {"value": 35},
+                "num_snv_bases": {"value": 11},
+                "snv_rate": {"value": 0.3142857},
+                "snv_per_diploid": {"value": 1905743997},
             },
         ),
         (
             "tests/data/test_ex_somatic_variant_rate/filtered_variants.vcf",
             {
-                "starting_bases": 32,
-                "min-BQ": 20,
-                "min-MQ": 30,
-                "filtered_bases": 9,
-                "evaluated_bases": 23,
-                "num_snv_bases": 15,
-                "snv_rate": 0.6521739,
-                "snv_per_diploid": 3954607210
-            }
+                "starting_bases": {"value": 32},
+                "min-BQ": {"value": 20},
+                "min-MQ": {"value": 30},
+                "filtered_bases": {"value": 9},
+                "evaluated_bases": {"value": 23},
+                "num_snv_bases": {"value": 15},
+                "snv_rate": {"value": 0.6521739},
+                "snv_per_diploid": {"value": 3954607210},
+            },
         ),
         (
             "tests/data/test_ex_somatic_variant_rate/no_variants.vcf",
             {
-                "starting_bases": 0,
-                "min-BQ": 20,
-                "min-MQ": 30,
-                "filtered_bases": 0,
-                "evaluated_bases": 0,
-                "num_snv_bases": 0,
-                "snv_rate": 0,
-                "snv_per_diploid": 0,
-            }
+                "starting_bases": {"value": 0},
+                "min-BQ": {"value": 20},
+                "min-MQ": {"value": 30},
+                "filtered_bases": {"value": 0},
+                "evaluated_bases": {"value": 0},
+                "num_snv_bases": {"value": 0},
+                "snv_rate": {"value": 0},
+                "snv_per_diploid": {"value": 0},
+            },
         ),
     ]
 )
+
 def test_somatic_variant_rate(tmp_path, vcf_path, expected_metrics):
     output_file = tmp_path / "results.json"
     log_file = tmp_path / "log.txt"
@@ -77,27 +78,21 @@ def test_somatic_variant_rate(tmp_path, vcf_path, expected_metrics):
     metrics = {}
     with open(output_file) as f:
         metrics = json.load(f)
-        metrics["starting_bases"] = int(metrics["starting_bases"])
-        metrics["filtered_bases"] = int(metrics["filtered_bases"])
-        metrics["evaluated_bases"] = int(metrics["evaluated_bases"])
-        metrics["num_snv_bases"] = int(metrics["num_snv_bases"])
-        metrics["snv_rate"] = float(metrics["snv_rate"])
-        metrics["snv_per_diploid"] = float(metrics["snv_per_diploid"])
     
-    if metrics["min-BQ"] != "NA":
-        metrics["min-BQ"] = int(metrics["min-BQ"])
-    if metrics["min-MQ"] != "NA":
-        metrics["min-MQ"] = int(metrics["min-MQ"])
+    if metrics["min-BQ"]["value"] != "NA":
+        metrics["min-BQ"]["value"] = int(metrics["min-BQ"]["value"])
+    if metrics["min-MQ"]["value"] != "NA":
+        metrics["min-MQ"]["value"] = int(metrics["min-MQ"]["value"])
     
     # Test if metrics match expected values
-    assert metrics["starting_bases"] == expected_metrics["starting_bases"]
-    assert metrics["min-BQ"] == expected_metrics["min-BQ"]
-    assert metrics["min-MQ"] == expected_metrics["min-MQ"]
-    assert metrics["filtered_bases"] == expected_metrics["filtered_bases"]
-    assert metrics["evaluated_bases"] == expected_metrics["evaluated_bases"]
-    assert metrics["num_snv_bases"] == expected_metrics["num_snv_bases"]
-    assert pytest.approx(metrics["snv_rate"], rel=1e-6) == expected_metrics["snv_rate"]
-    assert pytest.approx(metrics["snv_per_diploid"], rel=1e-4) == expected_metrics["snv_per_diploid"]
+    assert metrics["starting_bases"]["value"] == expected_metrics["starting_bases"]["value"]
+    assert metrics["min-BQ"]["value"] == expected_metrics["min-BQ"]["value"]
+    assert metrics["min-MQ"]["value"] == expected_metrics["min-MQ"]["value"]
+    assert metrics["filtered_bases"]["value"] == expected_metrics["filtered_bases"]["value"]
+    assert metrics["evaluated_bases"]["value"] == expected_metrics["evaluated_bases"]["value"]
+    assert metrics["num_snv_bases"]["value"] == expected_metrics["num_snv_bases"]["value"]
+    assert pytest.approx(metrics["snv_rate"]["value"], rel=1e-6) == expected_metrics["snv_rate"]["value"]
+    assert pytest.approx(metrics["snv_per_diploid"]["value"], rel=1e-4) == expected_metrics["snv_per_diploid"]["value"]
 
     if os.path.exists(str(log_file)):
         os.remove(str(log_file))
