25c210592addf9ba0a996b03749d38cac20c7e42
Cameron Fraser <cameron.fraser@systematicmedicine.com>
2026-02-24 10:17:43 +1100

    Flattened structure of paths package to make imports more intuitive and less verbose

--- definitions/paths/io/ex.py
+++ definitions/paths/io/ex.py
@@ -20,11 +20,11 @@ UMIXD_FASTQ_R2 = "tmp/{ex_lane}/{ex_lane}_r2_umi_extracted.fastq.gz"
 DEMUXD_FASTQ_R1 = "tmp/{ex_sample}/{ex_sample}_r1_demux.fastq.gz"
 DEMUXD_FASTQ_R2 = "tmp/{ex_sample}/{ex_sample}_r2_demux.fastq.gz"
 
-EX_TRIM_FASTQ_INT1_R1 = "tmp/{ex_sample}/{ex_sample}_r1_trim_adapters.fastq.gz"
-EX_TRIM_FASTQ_INT1_R2 = "tmp/{ex_sample}/{ex_sample}_r2_trim_adapters.fastq.gz"
+TRIM_FASTQ_INT1_R1 = "tmp/{ex_sample}/{ex_sample}_r1_trim_adapters.fastq.gz"
+TRIM_FASTQ_INT1_R2 = "tmp/{ex_sample}/{ex_sample}_r2_trim_adapters.fastq.gz"
 
-EX_TRIM_FASTQ_INT2_R1 = "tmp/{ex_sample}/{ex_sample}_r1_trim_adapters2.fastq.gz"
-EX_TRIM_FASTQ_INT2_R2 = "tmp/{ex_sample}/{ex_sample}_r2_trim_adapters2.fastq.gz"
+TRIM_FASTQ_INT2_R1 = "tmp/{ex_sample}/{ex_sample}_r1_trim_adapters2.fastq.gz"
+TRIM_FASTQ_INT2_R2 = "tmp/{ex_sample}/{ex_sample}_r2_trim_adapters2.fastq.gz"
 
 TRIMMED_FASTQ_R1 = "tmp/{ex_sample}/{ex_sample}_r1_trim.fastq.gz"
 TRIMMED_FASTQ_R2 = "tmp/{ex_sample}/{ex_sample}_r2_trim.fastq.gz"
