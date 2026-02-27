9490ecd9bd5720782c75d35f3b9a5b0d3e268a67
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2026-02-27 14:09:49 +1100

    Working on tests for ms pileup and germline risk

--- tests/scripts/ms/test_ms_low_depth.py
+++ tests/scripts/ms/test_ms_low_depth.py
@@ -9,6 +9,7 @@ Authors:
 """
 from pathlib import Path
 from helpers.get_metadata import load_config, get_ms_sample_ids
+from definitions.paths.io import ms as MS
 
 # Test that low depth BED has the correct structure
 def test_bed_structure_correct(lightweight_test_run):
@@ -16,7 +17,7 @@ def test_bed_structure_correct(lightweight_test_run):
     ms_samples = get_ms_sample_ids(config)
 
     for ms_sample in ms_samples:
-        bed_file = Path(f"tmp/{ms_sample}/{ms_sample}_lowdepth.bed")
+        bed_file = Path(MS.LOW_DEPTH_MASK.format(ms_sample=ms_sample))
 
         with bed_file.open() as f:
             for linenum, line in enumerate(f, start=1):
@@ -28,4 +29,4 @@ def test_bed_structure_correct(lightweight_test_run):
                 end = int(cols[2])
 
                 # Assertion 2: Start position is before end position
-                assert start < end, f"Start >= end on line {linenum}: {line}"    
+                assert start < end, f"Start >= end on line {linenum}: {line}"
