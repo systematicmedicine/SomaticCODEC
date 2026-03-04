225ac1f63e86b34c31c6e4410d5b25962ae7730d
JoshuaJohnstone <joshua.johnstone@systematicmedicine.com>
2026-03-04 14:00:08 +1100

    Started replacing hardcoded file paths in tests

--- tests/scripts/ex/test_ex_add_read_groups.py
+++ tests/scripts/ex/test_ex_add_read_groups.py
@@ -8,24 +8,39 @@ Authors:
     - Joshua Johnstone
 """
 from pathlib import Path
-import glob
 from helpers.bam_helpers import count_bam_data_points
+from helpers.get_metadata import load_config, get_ex_sample_ids
+import definitions.paths.io.ex as EX
 
-# Test that the read counts don't change during annotation
+# Test that the read counts don't change during adding read groups
 def test_reads_decrease(lightweight_test_run):
-    # Locate all pre-annotation BAM files
-    pre_files = glob.glob("tmp/*/*_map_correct.bam")
+    
+    # Load ex_sample IDs
+    config = load_config(lightweight_test_run["test_config_path"])
+    ex_samples = get_ex_sample_ids(config)
+
+    # Locate all pre-read group BAM files
+    pre_files = []
+    for ex_sample in ex_samples:
+        resolved_path = EX.FILTERED_BAM.format(ex_sample=ex_sample)
+        pre_files.append(resolved_path)
+
+    # Locate all post-read group BAM files
+    post_files = []
+    for ex_sample in ex_samples:
+        resolved_path = EX.READ_GROUP_BAM.format(ex_sample=ex_sample)
+        post_files.append(resolved_path)
+
+    # Count reads pre- and post- adding read groups
     pre_counts = {Path(f).name: count_bam_data_points(f) for f in pre_files}
     total_pre_reads = sum(pre_counts.values())
 
-    # Locate all post-annotation BAM files
-    post_files = glob.glob("tmp/*/*_map_read_group.bam")
     post_counts = {Path(f).name: count_bam_data_points(f) for f in post_files}
     total_post_reads = sum(post_counts.values())
 
-    # Assert total reads post annotation == total reads pre annotation
+    # Assert total reads post read group == total reads pre read group
     assert total_post_reads == total_pre_reads, (
-        f"Post-annotation reads ({total_post_reads}) != pre-annotation reads ({total_pre_reads})"
+        f"Post-read group reads ({total_post_reads}) != pre-read group reads ({total_pre_reads})"
     )
 
 
